267866335129f23fad68a9dd36cc97340bb2556b
Sabarish-2 <sabarishh03@gmail.com>
2024-03-04 07:25:18 +0530

    Update package.json and Homepage.tsx, remove react-app-env.d.ts

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,11 @@
 function HomePage() {
   return (
     <div className="App">
-      Hello, World
-    </div >
+      <h1 style={{ fontSize: '3rem', color: 'purple', fontWeight: 'bold' }}>Welcome to <em>The Money Concern</em> Tax Calculator</h1>
+      <p style={{ fontSize: '2rem', color: 'blue', fontStyle: 'italic' }}>Calculate your taxes with ease and accuracy.</p>
+      <p style={{ fontSize: '2rem', color: 'orange', textDecoration: 'bold' }}>Explore more options to calculate taxes in the navigation bar.</p>
+      <p style={{ fontSize: '2rem', color: 'green', textDecoration: 'underline' }}>Stay tuned for the upcoming tax deduction feature.</p>
+    </div>
   );
 }
 
